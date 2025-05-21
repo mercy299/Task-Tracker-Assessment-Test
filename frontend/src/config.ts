51c108d65145b2d8953ffba558b594d1e4ad3825
mercy299 <mercysam299@gmail.com>
2025-05-21 18:32:51 +0100

    Config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,6 @@
 const API_HOST = import.meta.env.VITE_API_HOST || 'localhost';
 const API_PORT = import.meta.env.VITE_API_PORT || '3001';
 const API_URL =
-  import.meta.env.VITE_API_URL || `http://${API_HOST}:${API_PORT}/api`;
+  `${import.meta.env.VITE_API_URL}/api` || `http://${API_HOST}:${API_PORT}/api`;
 
 export { API_URL };
